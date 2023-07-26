085761d7f85730c564c9fb6d2a35b52bbabdd772
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-07-26 04:21:40 -0400

    next&Prev button added

--- src/Components/News.js
+++ src/Components/News.js
@@ -12,11 +12,41 @@ export class News extends Component {
         }
     }
     async componentDidMount(){
-        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=fb304738285147ebad10678ed043ca99&page=1";
+        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=fb304738285147ebad10678ed043ca99&page=1&pageSize=20";
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
-        this.setState({articles: parsedData.articles})
+        this.setState({articles: parsedData.articles,totalArticles:parsedData.totalResults})
+
+    }
+
+    handlePrevClick=async()=>{
+      
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=fb304738285147ebad10678ed043ca99&page=${this.state.page-1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page:this.state.page-1,
+            articles: parsedData.articles,
+        })
+   
+
+    }
+    handleNextClick=async()=>{
+       if(this.state.page + 1>Math.ceil(this.state.totalResults/20)){
+
+       }
+       else{
+        let url=`https://newsapi.org/v2/top-headlines?country=in&apiKey=fb304738285147ebad10678ed043ca99&page=${this.state.page + 1}&pageSize=20`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page:this.state.page+1,
+            articles: parsedData.articles,
+        })
+    }
 
     }
 
@@ -34,8 +64,10 @@ export class News extends Component {
             
             
         </div>
-        <button type="button" class="btn btn-dark">Previous</button>
-        <button type="button" class="btn btn-dark">Next</button>
+        <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+            <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
       </div>
     )
   }
