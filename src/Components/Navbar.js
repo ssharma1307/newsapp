c328336265af0dfbcf4482ca8b2b627f11349ce0
ssharma1307 <sharma.sukhdeep30@gmail.com>
2023-07-26 22:09:54 -0400

    news categories updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
 
 export class Navbar extends Component {
   static propTypes = {
@@ -11,25 +12,27 @@ export class Navbar extends Component {
             <div>
                 <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
         <div className="container-fluid">
-            <a className="navbar-brand" href="/">News Nerd</a>
+            <Link className="navbar-brand" to="/">News Nerd</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/Home">Home</a>
-                </li>
-                <li className="nav-item">
-                <a className="nav-link" href="/About">About</a>
-                </li>
-               
+                <li className="nav-item"><Link className="nav-link " aria-current="page" to="/Home">Home</Link> </li>
+                <li className="nav-item"> <Link className="nav-link" to="/business">Business</Link></li>
+                <li className="nav-item"> <Link className="nav-link" to="/entertainment">Entertainment</Link></li>
+                <li className="nav-item"> <Link className="nav-link" to="/general">General</Link></li>
+                <li className="nav-item"> <Link className="nav-link" to="/health"> Health</Link></li>
+                <li className="nav-item"> <Link className="nav-link" to="/science">Science</Link></li>
+                <li className="nav-item"> <Link className="nav-link" to="/sports">Sports</Link></li>
+                <li className="nav-item"> <Link className="nav-link" to="/technology">Technology</Link></li>
+                              
                 
             </ul>
-            <form className="d-flex" role="search">
+            {/* <form className="d-flex" role="search">
                 <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
                 <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
+            </form> */}
             </div>
         </div>
         </nav>
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
-  render() {
-    
-    let {title,description,imageUrl,newsUrl} = this.props;
-    return (
-     <div className='my-3'> 
-            <div className="card" style={{width: "18rem"}}>
-        <img src={!imageUrl?"https://images.hindustantimes.com/tech/img/2023/07/25/1600x900/Instagram_1670246272938_1690255117611.jpg":imageUrl} className="card-img-top" alt="..."/>
-        <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-dark btn-sm">Read More</a>
-        </div>
-    </div>
-      </div>
-    )
-  }
+    render() {
+        let {title, description, imageUrl, newsUrl} = this.props;
+        return (
+            <div className="my-3">
+                <div className="card" >
+                <img src={!imageUrl?"https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg":imageUrl} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default NewsItem
